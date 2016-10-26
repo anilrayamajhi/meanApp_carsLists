1985afa24040bd0086de66b6c47d43796c4d6e46
Anil Rayamajhi <anilrm07@gmail.com>
2016-10-25 19:50:18 -0700

    working

--- client/js/controllers/carCtrl.js
+++ client/js/controllers/carCtrl.js
@@ -5,6 +5,7 @@ SingleCarController.$inject = ['$http', '$stateParams', '$state', 'CarFactory']
 
 function SingleCarController($http, $stateParams, $state, CarFactory){
   var vm = this;
+  vm.selected = false;
   // console.log($stateParams);
   // $http.get('api/cars/'+$stateParams.id)
   CarFactory.show($stateParams.id)
@@ -13,10 +14,24 @@ function SingleCarController($http, $stateParams, $state, CarFactory){
       // console.log(vm.car);
     })
 
+// $http.delete('api/cars/'+$stateParams)
   vm.destroyCar = function(){
     CarFactory.destroy(vm.car._id)
       .success(function(data){
         $state.go('cars')
+      })
+  }
+
+  vm.launchEdit = function(){
+    vm.selected = true;
+    console.log(vm.car);
+  }
+
+  vm.editCar = function(){
+    // console.log();
+    $http.patch('api/cars/'+$stateParams.id, vm.car)
+      .success(function(data){
+        vm.selected = false;
       })
   }
 }
