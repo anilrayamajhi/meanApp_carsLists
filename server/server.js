6743a9a0cef9897d354ff1e4e748db232e80061c
Anil Rayamajhi <anilrm07@gmail.com>
2016-10-26 19:33:41 -0700

    last attempt for gh-pages

--- server/server.js
+++ server/server.js
@@ -4,12 +4,18 @@ var
   logger = require('morgan'),
   mongoose = require('mongoose'),
   bodyParser = require('body-parser'),
-  apiRoutes = require('./routes/cars.js')
+  apiRoutes = require('./routes/cars.js'),
+  ghpages = require('gh-pages'),
+  path = require('path'),
   PORT = process.env.port || 3000
 
 mongoose.connect('mongodb://localhost/factories-practice', function(err) {
   console.log(err || "Connected to MongoDB (factories-practice)")
 })
+
+ghpages.publish(path.join('/client/index.html'), function(err) {
+  if(err) throw err;
+});
 
 app.use(logger('dev'))
 app.use(bodyParser.json())
