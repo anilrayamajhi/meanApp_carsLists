21256a0a1e8ceb3833df9bd6210ffe31499d70a1
Anil Rayamajhi <anilrm07@gmail.com>
2017-02-13 14:11:16 -0800

    Updates

--- controllers/cars.js
+++ controllers/cars.js
@@ -21,7 +21,7 @@ function index(req, res) {
 function show(req, res) {
   Car.findById(req.params.id, function(err, car) {
     if(err) return console.log('Error', err)
-    console.log(car);
+    console.log(car._id);
     res.json(car)
   })
 }
