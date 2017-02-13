87c9ff3d634b04d3318ecaeab215db7f6c1873d6
Anil Rayamajhi <anilrm07@gmail.com>
2017-02-13 14:35:38 -0800

    Updates

--- controllers/cars.js
+++ controllers/cars.js
@@ -22,9 +22,9 @@ function index(req, res) {
 function show(req, res) {
   Car.findById(req.params.id, function(err, car) {
     if(err) {
+    res.sendFile(path.normalize(__dirname + '/../../client/index.html'), {root: './'})
     console.log('ERROR', err);
     console.log('PATH',path.normalize(__dirname + '/../../client/index.html'));
-    res.sendFile(path.normalize(__dirname + '/../../client/index.html'), {root: './'})
   };
     res.json(car)
   })
