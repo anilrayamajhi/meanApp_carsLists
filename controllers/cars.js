ee72e548c65ad1b32b22ab08ac2448b5da4bec17
Anil Rayamajhi <anilrm07@gmail.com>
2017-02-13 15:04:04 -0800

    Updates

--- controllers/cars.js
+++ controllers/cars.js
@@ -1,7 +1,11 @@
 var
   Car = require('../models/Car.js'),
+  path = require('path'),
+  express = require('express'),
+  app = express(),
   seeds = require('../seeds.js')
 
+// app.use(express.static('client'))
 
 module.exports = {
   index,
@@ -21,7 +25,10 @@ function index(req, res) {
 
 function show(req, res) {
   Car.findById(req.params.id, function(err, car) {
-    if(err) return  console.log(err);
+    if(err) {
+      console.log('ERROR', err);
+      res.json
+  };
     res.json(car)
   })
 }
